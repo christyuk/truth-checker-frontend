5f29abad2778e8d4c6bea0f4271f39bd32381861
christyuk <christyzenzone78@gmail.com>
2026-01-05 08:39:25 +0530

    Make demo public – remove login requirement

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TruthCheck from "./pages/TruthCheck";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Demo route – opens directly */}
+        {/* Public demo page */}
         <Route path="/" element={<TruthCheck />} />
-
-        {/* Keep these for code completeness (not required for demo) */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
